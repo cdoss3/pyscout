e76ed3a29da1fcf9fc4e1a104d61256931f7dd39
cdoss3 <christopher.doss92@gmail.com>
2024-04-16 13:29:32 -0500

    clean_player_data.py can now accept cli arguments for input and output of files

--- clean_player_data.py
+++ clean_player_data.py
@@ -4,8 +4,16 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Clean Wyscout player data. Provide a .csv file to be cleaned, output file in /pyscout/datasets')
 
-parser.add_argument('-f', '--file', help="REQUIRED -- Provide the pato the .csv file to be cleaned", default=None)
+parser.add_argument('-f', '--file', help="REQUIRED -- Provide the path to the .csv file to be cleaned", default=None)
+parser.add_argument('-o', '--output', help="REQUIRED -- Provide the path to the output .csv file", default=None)
 
 args = parser.parse_args()
 
-print(args.file + ' Provided')
+with open(args.file, 'r') as file:
+    lines = [line for line in file.readlines()]
+    for line in lines:
+        print(line)
+
+with open(args.output, 'w') as file:
+    file.write('Does this print to an output file?')
+
