3ccf27bc88f34bdcd21d0bfd9e45c6868ec63029
cdoss3 <christopher.doss92@gmail.com>
2024-09-10 21:55:33 -0500

    Finished DataFrame construction for individual player analysis. Fixed data cleaner. Some quotes are double still, some things need factoring, but I'm hungry

--- clean_player_data.py
+++ clean_player_data.py
@@ -20,17 +20,50 @@ df = pd.read_excel(args.file)
 # Define a function to clean the column names. Then, execute the function on the dataframe.
 
 def data_clean(df):
-    df.drop(['Team', 'Weight', 'Height'], axis=1, inplace=True)
-    df.columns = [x.lower().replace(', %', ' pct').replace(', m', ' (m)').replace(' / ', '/').replace('non-penalty', 'non pen') for x in df.columns]
     df.rename(columns={
-        'player': 'name',
-        'team within selected timeframe': 'team',
-        'age': 'birth year',
-        'matches played': 'appearances',
-        'minutes played': 'minutes', 
-        'back passes received as gk per 90': 'gk passes received',
-        'aerial duels per 90.1': 'aerial duels per 90',
-    }, inplace=True)
+        'Total actions / successful': 'Total actions',
+        'Unnamed: 6': 'Successful actions',
+        'Shots / on target': 'Shots',
+        'Unnamed: 10': 'Shots on target',
+        'Passes / accurate': 'Passes attempted',
+        'Unnamed: 13': 'Passes completed',
+        'Long passes / accurate': 'Long passes attempted',
+        'Unnamed: 15': 'Long passes completed',
+        'Crosses / accurate': 'Crosses attempted',
+        'Unnamed: 17': 'Crosses completed',
+        'Dribbles / successful': 'Dribbles attempted',
+        'Unnamed: 19': 'Dribbles completed',
+        'Duels / won': 'Duels attempted',
+        'Unnamed: 21': 'Duels won',
+        'Aerial duels / won': 'Aerial duels attempted',
+        'Unnamed: 23': 'Aerial duels won',
+        'Losses / own half': 'Lost possession',
+        'Unnamed: 26': 'Lost possession in own half',
+        'Recoveries / opp. half': 'Recovered ball',
+        'Unnamed: 28': 'Recovered ball in opp half',
+        'Defensive duels / won': 'Defensive duels',
+        'Unnamed: 32': 'Defensive duels won',
+        'Loose ball duels / won': 'Loose ball duels',
+        'Unnamed: 34': 'Loose ball duels won',
+        'Sliding tackles / successful': 'Sliding tackles attempted',
+        'Unnamed: 36': 'Sliding tackles successful',
+        'Offensive duels / won': 'Offensive duels attempted',
+        'Unnamed: 43': 'Offensive duels won',
+        'Through passes / accurate': 'Through balls attempted',
+        'Unnamed: 49': 'Through balls completed',
+        'Passes to final third / accurate': 'Passes to final third attempted',
+        'Unnamed: 53': 'Passes to final third successful',
+        'Passes to penalty area / accurate': 'Passes to penalty area attempted',
+        'Unnamed: 55': 'Passes to penalty area successful',
+        'Forward passes / accurate': 'Forward passes attempted',
+        'Unnamed: 58': 'Forward passes completed',
+        'Back passes / accurate': 'Back passes attempted',
+        'Unnamed: 60': 'Back passes completed',
+        'Saves / with reflexes': 'Saves',
+        'Unnamed: 65': 'Reflex saves',
+        'Passes to GK / accurate': 'Passes to GK attempted',
+        'Unnamed: 68': 'passes to GK completed',
+}, inplace=True)
 
 data_clean(df)
 
@@ -42,9 +75,3 @@ for column in df.columns:
 # Output dataframe to .csv file
 
 df.to_csv(args.output)
-
-# Print column names to .txt file for reference
-
-with open('column_names.txt', 'w') as file:
-    for column in df.columns:
-        file.write(column + '\n')
