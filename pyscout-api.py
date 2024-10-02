5058bc0d28172f7f2c06ebbde855e37a5c0946b9
cdoss3 <christopher.doss92@gmail.com>
2024-10-01 19:10:09 -0500

    Initialized pyscout-api and downloaded Wyscout event-level data in V3Events.txt

---
+++ pyscout-api.py
@@ -0,0 +1,14 @@
+#! /bin/python3
+#
+# This is to test event-data functionality with the Wyscout API. We have the demo
+# data from their website, titled "V3Events.txt", our goal is to read the feed
+# as a .json file, and output events correctly as they happen.
+#
+# We'll eventually use this functionality to build out a full API reader,
+# and use that to build more fancy things.
+
+def main():
+    pass
+
+if __name__=="__main__":
+    print("Hello world")
